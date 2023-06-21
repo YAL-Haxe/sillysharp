47809783216f9ef7ded51e9aa0b9a3724886ed31
YellowAfterlife <yellowafterlife@hotmail.com>
2023-06-21 16:23:55 +0300

    Support flat enums

--- src/sf/type/SfEnum.hx
+++ src/sf/type/SfEnum.hx
@@ -1,5 +1,28 @@
 package sf.type;
+import sf.type.SfBuffer;
+import sf.SfCore.*;
 
 class SfEnum extends SfEnumImpl {
-	//
+	override public function printTo(out:SfBuffer, init:SfBuffer):Void {
+		if (isHidden) return;
+		if (pack.length > 0) {
+			var _pack = pack.map(sfGenerator.getVarName);
+			printf(out, "namespace %s;\n", _pack.join("."));
+		}
+		printf(out, "enum %s {%(+\n)", name);
+		var sep = false;
+		for (ctr in ctrList) {
+			inline function addSep() {
+				if (sep) {
+					printf(out, "\n\n");
+				} else {
+					sep = true;
+				}
+				out.addSummaryComment(ctr.doc);
+			}
+			addSep();
+			printf(out, "%s,", ctr.name);
+		}
+		printf(out, "%(-\n)}\n");
+	}
 }
