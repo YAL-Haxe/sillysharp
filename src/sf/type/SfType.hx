c32b112a428e8ed817670faa4759749d4037a2e0
YellowAfterlife <yellowafterlife@hotmail.com>
2023-06-21 16:15:05 +0300

    Proper detection for System.*/System.Collections.Generic.* types

--- src/sf/type/SfType.hx
+++ src/sf/type/SfType.hx
@@ -2,11 +2,16 @@ package sf.type;
 import haxe.macro.Type.BaseType;
 
 class SfType extends SfTypeImpl {
+	public var isCsNative:Bool;
+	public var isSys:Bool;
 	public var isSysGeneric:Bool;
 	public function new(t:BaseType) {
 		super(t);
+		isCsNative = t.meta.has(":csNative");
+		isSys = t.pack[0] == "cs" && t.pack[1] == "system" || t.pack[0] == "System";
 		isSysGeneric = switch (t.pack) {
 			case ["cs", "system", "collections", "generic"]: true;
+			case ["System", "Collections", "Generic"]: true;
 			default: false;
 		}
 	}
